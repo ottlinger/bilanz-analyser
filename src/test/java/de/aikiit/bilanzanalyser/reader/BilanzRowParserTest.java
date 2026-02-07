49da232c4e2c426b10ebf90f9fbf5538804be905
Hugo Hirsch <github@hugo-hirsch.de>
2026-02-07 12:07:12 +0100

    #3: Add example data in tests

--- src/test/java/de/aikiit/bilanzanalyser/reader/BilanzRowParserTest.java
+++ src/test/java/de/aikiit/bilanzanalyser/reader/BilanzRowParserTest.java
@@ -1,15 +1,33 @@
 package de.aikiit.bilanzanalyser.reader;
 
 import org.junit.jupiter.api.Test;
+import org.odftoolkit.odfdom.doc.OdfDocument;
+import org.odftoolkit.odfdom.doc.OdfSpreadsheetDocument;
+import org.odftoolkit.odfdom.doc.table.OdfTable;
+import org.odftoolkit.odfdom.doc.table.OdfTableRow;
+
+import java.util.Optional;
 
 import static de.aikiit.bilanzanalyser.reader.BilanzRowParser.cleanUpAmount;
+import static de.aikiit.bilanzanalyser.reader.BilanzRowParser.fromOdfTableRow;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BilanzRowParserTest {
 
     @Test
-    void fromOdfTableRow() {
-        // tbd
+    void fromOdfTableRowNPESafe() throws Exception {
+        assertThat(fromOdfTableRow(null)).isEmpty();
+        assertThat(fromOdfTableRow(createExampleRow())).isEmpty();
+    }
+
+    private static OdfTableRow createExampleRow() throws Exception {
+        try (OdfDocument document = OdfSpreadsheetDocument.newSpreadsheetDocument()) {
+            final Optional<OdfTable> first = document.getTableList(true).stream().findFirst();
+            if (first.isPresent()) {
+                return first.get().appendRow();
+            }
+        }
+        throw new IllegalArgumentException("No table found");
     }
 
     @Test
