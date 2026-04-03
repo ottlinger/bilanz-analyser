781a2478804308733039847f574c910b09149d6d
Hugo Hirsch <github@hugo-hirsch.de>
2026-04-03 20:36:00 +0200

    #6: Show upload UI

--- src/main/java/de/aikiit/bilanzanalyser/upload/UploadController.java
+++ src/main/java/de/aikiit/bilanzanalyser/upload/UploadController.java
@@ -1,12 +1,24 @@
 package de.aikiit.bilanzanalyser.upload;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 @RestController
 public class UploadController {
     @GetMapping("/upload")
     public String upload() {
-        return "Hello, World!";
+        return "upload";
+    }
+
+    @PostMapping("/handle-upload")
+    public ResponseEntity<String> uploadFileRequestParam(File file) {
+        // TODO Handle the file upload logic here
+        // Save the file or it as needed
+        return ResponseEntity.ok("File uploaded successfully: " + file.getName());
     }
 }
