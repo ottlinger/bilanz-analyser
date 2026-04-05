239eccc7aef9c2cda3c527f8cc89a1e8fc99e4ec
Hugo Hirsch <github@hugo-hirsch.de>
2026-04-05 22:23:18 +0200

    #5: Try to load data items asynchronously on start page

---
+++ src/main/java/de/aikiit/bilanzanalyser/api/DataController.java
@@ -0,0 +1,19 @@
+package de.aikiit.bilanzanalyser.api;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class DataController {
+
+    @GetMapping("/dataitems")
+    public List<Pair<String, Integer>> getDataItems() {
+        // TODO replace with real data from database if done
+        return List.of(Pair.of("Apple", 1), Pair.of("Banana", 234));
+    }
+}
