005a56797a6fd71eaf6249c7c51f5dcec77630f0
Hugo Hirsch <github@hugo-hirsch.de>
2026-04-12 21:40:52 +0200

    #12: Add basic infra

---
+++ site.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Generating new mvn site ..."
+# ./mvnw -B clean test-compile org.pitest:pitest-maven:mutationCoverage site:site 
+./mvnw -B clean install org.pitest:pitest-maven:mutationCoverage site:site 
+echo "DONE - ready to commit and push"
+cp -rf target/site/* docs
+git add -f docs/
+git commit -a -m "Issue #12: Update site"
+# DEVHINT: This may fail if any changes/commits were run in the background on the repo, e.g. dependabot/GHA automation scripts
+git push
