3f5d22069a6a3f5e67e0a4f7a3c3ac29da23310b
Hugo Hirsch <github@hugo-hirsch.de>
2026-04-18 21:45:09 +0200

    Fix Checkstyle warning

--- src/main/java/de/aikiit/bilanzanalyser/WebAppApplication.java
+++ src/main/java/de/aikiit/bilanzanalyser/WebAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class WebAppApplication {
+public class WebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppApplication.class, args);
