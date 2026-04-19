4b0750587715833a9324af7ca3654ddf8ae29583
Hugo Hirsch <github@hugo-hirsch.de>
2026-04-19 22:40:48 +0200

    #5: Remove unused imports

--- src/main/java/de/aikiit/bilanzanalyser/upload/UploadController.java
+++ src/main/java/de/aikiit/bilanzanalyser/upload/UploadController.java
@@ -1,10 +1,8 @@
 package de.aikiit.bilanzanalyser.upload;
 
-import de.aikiit.bilanzanalyser.entity.database.repository.SourceRepository;
 import de.aikiit.bilanzanalyser.reader.BilanzRowParserResult;
 import de.aikiit.bilanzanalyser.reader.BilanzRowParserStatistic;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +17,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 @Log4j2
 @RestController
