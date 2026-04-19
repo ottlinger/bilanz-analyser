c8ab8975ffb0494412cbfa32946d849f46f9ec4d
Hugo Hirsch <github@hugo-hirsch.de>
2026-04-19 12:18:45 +0200

    #5: Log execution time

--- src/main/java/de/aikiit/bilanzanalyser/upload/UploadAnalysisService.java
+++ src/main/java/de/aikiit/bilanzanalyser/upload/UploadAnalysisService.java
@@ -2,6 +2,7 @@ package de.aikiit.bilanzanalyser.upload;
 
 import de.aikiit.bilanzanalyser.reader.BilanzOdsReader;
 import de.aikiit.bilanzanalyser.reader.BilanzRowParserResult;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +10,15 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 @Service
+@Log4j2
 public class UploadAnalysisService {
 
     /**
      * Parses and analyses a given file.
      *
      * @param worksheetName selected worksheet name to process.
-     * @return result container.
      * @param spreadsheet   path to spreadsheet file.
+     * @return result container.
      * @throws IOException in case of I/O problems.
      */
     BilanzRowParserResult processFile(String worksheetName, Path spreadsheet) throws IOException {
@@ -26,7 +28,11 @@ public class UploadAnalysisService {
 
     @Async
     public void flushDataIntoDatabase(BilanzRowParserResult data) {
+        long start = System.nanoTime();
+        log.info("Starting to flush data into database...");
+
         // TODO #5 long-running task parsing, DB work, calling external APIs
+        log.info("Successfully flushed data into database in {} ms.", (System.nanoTime() - start) / 1_000_000);
     }
 
 }
